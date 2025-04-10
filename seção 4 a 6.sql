fb2ec3ee95e7fc5fa85ba074673f2f2641d6cc6a
AnnaFAmorim <166024883+AnnaFAmorim@users.noreply.github.com>
2025-04-10 08:15:56 -0300

    Update seção 4 a 6.sql

--- seção 4 a 6.sql
+++ seção 4 a 6.sql
@@ -1,60 +1,61 @@
+--Questão 1:
 SELECT UPPER(cliente_nome) AS nome_maiusculo
 FROM cliente;
 
--- Questão 2: Nome dos clientes com apenas a primeira letra em maiúscula
+--Questão 2:
 SELECT INITCAP(cliente_nome) AS nome_formatado
 FROM cliente;
 
--- Questão 3: Três primeiras letras do nome de cada cliente
+--Questão 3: 
 SELECT SUBSTR(cliente_nome, 1, 3) AS abreviacao
 FROM cliente;
 
--- Questão 4: Número de caracteres do nome de cada cliente
+--Questão 4: 
 SELECT cliente_nome, LENGTH(cliente_nome) AS comprimento
 FROM cliente;
 
--- Questão 5: Saldo das contas arredondado para o inteiro mais próximo
+--Questão 5:
 SELECT conta_numero, ROUND(saldo) AS saldo_arredondado
 FROM conta;
 
--- Questão 6: Saldo das contas truncado (sem casas decimais)
+--Questão 6: 
 SELECT conta_numero, TRUNC(saldo) AS saldo_truncado
 FROM conta;
 
--- Questão 7: Resto da divisão do saldo por 1000
+--Questão 7: 
 SELECT conta_numero, MOD(saldo, 1000) AS resto_por_1000
 FROM conta;
 
--- Questão 8: Data atual do sistema
+--Questão 8: 
 SELECT SYSDATE AS data_atual FROM dual;
 
--- Questão 9: Data atual acrescida de 30 dias
+--Questão 9:
 SELECT SYSDATE + 30 AS data_vencimento FROM dual;
 
--- Questão 10: Dias entre a data de abertura da conta e a data atual
+--Questão 10:
 SELECT conta_numero, (SYSDATE - data_abertura) AS dias_de_uso
 FROM conta;
 
--- Questão 11: Saldo formatado como moeda
+--Questão 11: 
 SELECT TO_CHAR(saldo, 'L9G999D99') AS saldo_formatado
 FROM conta;
 
--- Questão 12: Data de abertura formatada como 'dd/mm/yyyy'
+--Questão 12: 
 SELECT TO_CHAR(data_abertura, 'dd/mm/yyyy') AS data_formatada
 FROM conta;
 
 
-Questão 13
+--Questão 13
 
 SELECT conta_numero, NVL(saldo, 0) AS saldo_corrigido
 FROM conta;
 
-Questão 14
+--Questão 14
 
 SELECT cliente_nome, NVL(cidade, 'Sem cidade') AS cidade
 FROM cliente;
 
-Questão 15
+--Questão 15
 
 SELECT cliente_nome,
   CASE cidade
@@ -64,20 +65,20 @@ SELECT cliente_nome,
   END AS regiao
 FROM cliente;
 
-Questão 16
+--Questão 16
 
 SELECT c.cliente_nome, ct.conta_numero, ct.saldo
 FROM cliente c
 JOIN conta ct ON c.cliente_cod = ct.cliente_cliente_cod;
 
-Questão 17
+--Questão 17
 
 SELECT c.cliente_nome, a.agencia_nome
 FROM cliente c
 JOIN conta ct ON c.cliente_cod = ct.cliente_cliente_cod
 JOIN agencia a ON ct.agencia_agencia_cod = a.agencia_cod;
 
-Questão 18
+--Questão 18
 SELECT a.agencia_nome, c.cliente_nome
 FROM agencia a
 LEFT JOIN conta ct ON a.agencia_cod = ct.agencia_agencia_cod
